a885edeef81bbbce2f4cf936c9c6b23452e9ca56
Sebastian Aristizabal <sebastinn@hotmail.com>
2023-05-09 22:39:17 -0500

    git under construction

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Register.destroy_all
+register1 = Register.new(first_name: "Sebastián", last_name: "Aristizábal Suárez", sex: description: "Las mejores colas de mono y Piscos de todo copiapó", address: "Calle de la Luna 13", city: "Copiapó", photo_url: "www.lewagon.com")
+register1.save!
+register2 = Register.new(name: "el tragadero", description: "las mejores butifarras", address: "calle 50", city: "medellín", photo_url: "www.lewagon.com")
+register2.save!
+puts "Restaurante creado"
+
+t.string "first_name"
+t.string "last_name"
+t.string "sex"
+t.date "birth_date"
+t.string "email"
+t.text "description"
+t.string "address"
+t.string "country"
+t.string "department"
+t.string "city"
+t.string "apartment"
