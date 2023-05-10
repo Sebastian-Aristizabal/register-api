a25f68ac4f2ca2e56f09b6b64dbc76998257f614
Sebastian Aristizabal <sebastinn@hotmail.com>
2023-05-10 05:08:06 -0500

    validate presence true all fields less apartment

--- app/models/api/v1/register.rb
+++ app/models/api/v1/register.rb
@@ -1,3 +1,13 @@
 class Api::V1::Register < ApplicationRecord
+  validates :first_name, :last_name, :gender, :birth_date,
+  :email, :description, :address, :country, :department, :city, presence: true
 
+
+  private
+
+  def validate_age
+    if birth_date.present? && birth_date > 18.years.ago.to_d
+        errors.add(:birth_date, 'You should be over 18 years old.')
+    end
+  end
 end
