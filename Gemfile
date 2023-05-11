d1fd67e7c05a16ef7a7f01696f81e95184193d62
Sebastian Aristizabal <sebastinn@hotmail.com>
2023-05-10 20:26:36 -0500

    implementation of an APi to bring all Regions and Cities from Colombia

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem "bootsnap", require: false
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem "rack-cors"
+gem 'soda-ruby', '~> 1.0', '>= 1.0.1'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
@@ -45,4 +46,3 @@ group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
 end
-
--- app/models/api/v1/register.rb
+++ app/models/api/v1/register.rb
@@ -4,12 +4,21 @@ class Api::V1::Register < ApplicationRecord
 
   validate :validate_legal_age
   before_save :validate_legal_age
+  validate :max_records_per_city
+  before_save :max_records_per_city
 
   private
 
   def validate_legal_age
     if self.birth_date.present? && self.birth_date.to_f > 18.years.ago.to_f
       self.errors.add(:birth_date, 'You should be over 18 years old.')
+    end
+  end
+
+  def max_records_per_city
+    city_count = Api::V1::Register.where(city: self.city).count
+    if city_count >= 3
+      errors.add(:city, "ha alcanzado el límite máximo de registros")
     end
   end
 end
