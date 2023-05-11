c7a7c2e5e1f1d517149e07b94e76ab800e3bc520
Sebastian Aristizabal <sebastinn@hotmail.com>
2023-05-10 19:40:48 -0500

    legal age validation working

--- app/models/api/v1/register.rb
+++ app/models/api/v1/register.rb
@@ -2,12 +2,14 @@ class Api::V1::Register < ApplicationRecord
   validates :first_name, :last_name, :gender, :birth_date,
   :email, :description, :address, :country, :department, :city, presence: true
 
+  validate :validate_legal_age
+  before_save :validate_legal_age
 
   private
 
-  def validate_age
-    if birth_date.present? && birth_date > 18.years.ago.to_d
-        errors.add(:birth_date, 'You should be over 18 years old.')
+  def validate_legal_age
+    if self.birth_date.present? && self.birth_date.to_f > 18.years.ago.to_f
+      self.errors.add(:birth_date, 'You should be over 18 years old.')
     end
   end
 end
